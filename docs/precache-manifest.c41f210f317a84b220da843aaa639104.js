e7c99c7f1c566f036e6c1c695cb544f23fcedd8c
minennai <ignaciominenna@gmail.com>
2022-01-03 21:27:17 -0300

    .

--- docs/precache-manifest.c41f210f317a84b220da843aaa639104.js
+++ docs/precache-manifest.c41f210f317a84b220da843aaa639104.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "d14dbffe069e4b3574e6f5ab32985a60",
-    "url": "/FIUBA-Map/index.html"
+    "url": "/FCE-Map/index.html"
   },
   {
     "revision": "0697c17c0ea8183a890a",
-    "url": "/FIUBA-Map/static/js/2.e0b5a456.chunk.js"
+    "url": "/FCE-Map/static/js/2.e0b5a456.chunk.js"
   },
   {
     "revision": "b45e14205663e3a520b3e0a455bba542",
-    "url": "/FIUBA-Map/static/js/2.e0b5a456.chunk.js.LICENSE.txt"
+    "url": "/FCE-Map/static/js/2.e0b5a456.chunk.js.LICENSE.txt"
   },
   {
     "revision": "eda7de5ff805146b6413",
-    "url": "/FIUBA-Map/static/js/main.23d9aa06.chunk.js"
+    "url": "/FCE-Map/static/js/main.23d9aa06.chunk.js"
   },
   {
     "revision": "15f20f19a13ea8d2a6c8",
-    "url": "/FIUBA-Map/static/js/runtime-main.9b100fd5.js"
+    "url": "/FCE-Map/static/js/runtime-main.9b100fd5.js"
   }
 ]);
